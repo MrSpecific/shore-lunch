4dff77806b253980e133135ea9fd178beeac8921
Will Christenson <will@wlcr.io>
2023-03-27 13:46:29 -0700

    Product card styling

--- components/commerce/ProductCard.tsx
+++ components/commerce/ProductCard.tsx
@@ -4,29 +4,36 @@ import { useShoppingCart } from 'use-shopping-cart';
 import styles from '@styles/components/ProductCard.module.css';
 
 const ProductCard = ({ product }) => {
-  const { name, price, currency } = product;
+  const { name, price, description, currency } = product;
   const { addItem, removeItem, handleCartHover } = useShoppingCart();
 
   return (
     <div className={styles.productCard}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={product.image} alt={name} className={styles.cardImage} />
-      <h2>{name}</h2>
-      <p className="price">
-        {formatCurrencyString({
-          value: price,
-          currency: currency,
-        })}
-      </p>
-      <button
-        className="button"
-        onClick={() => {
-          addItem(product);
-          handleCartHover();
-        }}
-      >
-        Add to cart
-      </button>
+      <div className={styles.cardTop}>
+        <img src={product.image} alt={name} className={styles.cardImage} />
+        <h2>{name}</h2>
+      </div>
+      <div className={styles.cardDetails}>
+        <div className={styles.price}>
+          {formatCurrencyString({
+            value: price,
+            currency: currency,
+          })}
+        </div>
+        <div className={styles.cardDescription}>{description}</div>
+      </div>
+      <div className={styles.cardActions}>
+        <button
+          className="button"
+          onClick={() => {
+            addItem(product);
+            handleCartHover();
+          }}
+        >
+          Add <span className="visually-hidden">{name}</span> to cart
+        </button>
+      </div>
     </div>
   );
 };
