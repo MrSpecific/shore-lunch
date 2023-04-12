600921987b8a07589ce84ed5e4272b4753afc128
Will Christenson <will@wlcr.io>
2023-04-11 21:44:09 -0700

    Fix gtag handling

--- lib/google.ts
+++ lib/google.ts
@@ -1,4 +1,7 @@
-export const gtagEvent = (eventName: string, eventParams?: object = {}) => {
+/// <reference types="gtag.js">
+declare const gtag: Gtag.Gtag;
+
+export const gtagEvent = (eventName: string, eventParams: object = {}) => {
   if (typeof gtag === 'function') {
     gtag('event', eventName, eventParams);
   }
