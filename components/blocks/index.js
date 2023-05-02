78609794b2a37e2452f973b6f5602284c2bd350d
Will Christenson <will@wlcr.io>
2023-05-01 21:36:10 -0700

    Fix barrel imports

--- components/blocks/index.js
+++
@@ -1,4 +0,0 @@
-export { default as FaqBlock } from './FaqBlock';
-export { default as ImageBlock } from './ImageBlock';
-export { default as ImageWithText } from './ImageWithText';
-export { default as TextBlock } from './TextBlock';
---
+++ components/layout/index.ts
@@ -0,0 +1,8 @@
+export { default as AnalyticsTags } from './AnalyticsTags';
+export { default as Content } from './Content';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Layout } from './Layout';
+export { default as MetaTags } from './MetaTags';
+export { default as Page } from './Page';
+export { default as SkipLink } from './SkipLink';
