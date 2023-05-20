47342511e4a92e16bbe9c752e50fd924058a9ae7
Will Christenson <will@wlcr.io>
2023-05-19 23:53:33 -0700

    Testing legend-state

--- pages/product/[permalink].js
+++ pages/product/[permalink].js
@@ -1,19 +1,32 @@
 import classNames from 'classnames';
 
+import { enableLegendStateReact, observer } from '@legendapp/state/react';
 import commerce from '@lib/commerce';
 import { Page } from '@layout';
 import BlocksGroup from '@components/BlocksGroup';
 import Hero from '@components/Hero';
 import styles from '@styles/page/EpisodePage.module.css';
-import { useCartDispatch, useCartState } from '@context/cart';
+import { useCartDispatch, useCartState, cart, cartState, cartCount } from '@context/cart';
 
 const { log } = console;
 
-export default function ProductPage({ product }) {
+enableLegendStateReact();
+
+export default observer(function ProductPage({ product }) {
   const { id, template, name, hero, blocks } = product || {};
   const { setCart } = useCartDispatch();
 
-  const addToCart = () => commerce.cart.add(id).then(({ cart }) => setCart(cart));
+  const addToCart = () =>
+    commerce.cart.add(id).then(({ cart }) => {
+      // cart.state.set(cart);
+      cartState.set(cart);
+      cartCount.set((v) => v + 1);
+      return setCart(cart);
+    });
+
+  log('Cart state', cart.state.get());
+  log('Cartstate', cartState.get());
+  log('cartCount', cartCount.get());
 
   const contentContainerClass = classNames({
     [styles.contentContainer]: true,
@@ -26,11 +39,14 @@ export default function ProductPage({ product }) {
         <div className={contentContainerClass}>
           <h1 className={styles.headline}>{name}</h1>
           <button onClick={addToCart}>Add to Cart</button>
+          <button onClick={() => cart.count.set((v) => v + 1)}>Increment 1</button>
+          <button onClick={() => cart.increment()}>Increment p</button>
+          {cart.state.id} - {cart.state.total_items} | {cartCount} - {cart.count}
         </div>
       </div>
     </Page>
   );
-}
+});
 
 export async function getStaticProps({ params }) {
   const { permalink } = params;
