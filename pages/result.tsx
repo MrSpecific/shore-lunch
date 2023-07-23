b22e724a87eaac59d2b63fc736edc31341b5eb93
Will Christenson <will@wlcr.io>
2023-07-22 21:49:33 -0700

    Style receipt page slightly

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,22 +1,29 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import { formatCurrencyString } from 'use-shopping-cart';
 
+import * as config from '@config';
 import { Page } from '@layout';
 import PrintObject from '@commerce/PrintObject';
 import Cart from '@commerce/Cart';
 import ClearCart from '@commerce/ClearCart';
-
 import { fetchGetJSON } from '@utils/apiHelpers';
-import useSWR from 'swr';
+import styles from '@styles/page/Result.module.css';
 
 const SuccessMessage = ({ data }) => {
-  const { customer_details, shipping_details, line_items } = data;
+  const { amount_total, currency, customer_details, shipping_details, line_items } = data;
+
   return (
     <div>
       <hr />
-      <h3>
-        Shipping to {shipping_details?.address?.city}, {shipping_details?.address?.state}
-      </h3>
+      Order total:&nbsp;
+      {formatCurrencyString({
+        value: amount_total,
+        currency: currency || config.CURRENCY,
+      })}
+      <br />
+      Shipping to {shipping_details?.address?.city}, {shipping_details?.address?.state}
       <hr />
       <h2>Thank you for your order!</h2>
     </div>
@@ -37,16 +44,19 @@ const ResultPage: NextPage = () => {
 
   return (
     <Page title="Checkout Result">
-      <div className="page-container">
-        <h1>Your Receipt.</h1>
-        {/* <h2>Status: {data?.payment_intent?.status ?? 'loading...'}</h2> */}
-        {data?.payment_intent?.status === 'succeeded' && <SuccessMessage data={data} />}
-        {/* <h3>CheckoutSession response:</h3> */}
-        {/* <PrintObject content={data ?? 'loading...'} /> */}
+      <section className="page-container">
+        <div className={styles.receiptContainer}>
+          <h1>Your Receipt.</h1>
+          {/* <h2>Status: {data?.payment_intent?.status ?? 'loading...'}</h2> */}
+          {data?.payment_intent?.status === 'succeeded' && <SuccessMessage data={data} />}
+          {/* <h3>CheckoutSession response:</h3> */}
+          {/* <PrintObject content={data ?? 'loading...'} /> */}
+        </div>
+
         <Cart>
           <ClearCart />
         </Cart>
-      </div>
+      </section>
     </Page>
   );
 };
