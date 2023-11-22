e8266eb2690acae0fdacc3ac787a792322e8b66f
Will Christenson <will@wlcr.io>
2023-11-22 14:24:51 -0800

    Updates

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  siteUrl: process.env.SITE_URL || 'https://pdx.coffeeoutside.org',
-  generateRobotsTxt: false, // (optional)
-  generateIndexSitemap: false,
-};
-
-// export default config;
-module.exports = config;
